4012fe467ce435ff9b93876c7884eef3d4426fad
Everkilk <tuannguyenlevan@gmail.com>
2024-04-26 05:13:23 +0700

    update product page and cart page

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,7 +1,12 @@
 let guest = JSON.parse(localStorage.getItem("guest"));
 if(guest.online == 1){
     $('#user').attr("href", "#");
-    $('#user div i').attr("class","fa-solid fa-user");
+    $('#user div i').attr("class","fa-solid fa-user log_out");
     $('#user div i').css("font-size","22px");
     $('#user div p').remove();
-}
+};
+
+$('.log_out').click(function(){
+    localStorage.clear();
+    location.reload();
+});
